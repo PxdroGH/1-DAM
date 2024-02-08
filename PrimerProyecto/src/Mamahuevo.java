61abcdcda5d832ed6f2dfd3d6cd9b2db0dba6499
PxdroGH <pedro99cuadrado@gmail.com>
2024-02-08 17:28:26 +0100

    Mamauevo

---
+++ PrimerProyecto/src/Mamahuevo.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class Mamahuevo {
+
+    public static void mamahuevo () {
+
+        System.out.println("Mamahuevo");
+    }
+}
